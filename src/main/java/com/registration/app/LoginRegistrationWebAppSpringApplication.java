df98865025eadc8daf1bc82896acdc28b7596767
unknown <sivamessiva10@gmail.com>
2023-03-30 22:58:22 +0530

    Initial Mannual Commit

---
+++ src/main/java/com/registration/app/LoginRegistrationWebAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.registration.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationWebAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationWebAppSpringApplication.class, args);
+	}
+
+}
